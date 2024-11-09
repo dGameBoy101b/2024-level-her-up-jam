e5b27d5100c491f489a76d3caf90898de1993589
dGameBoy101b <rhysmader@gmail.com>
2024-11-09 13:59:24 +0800

    Created card resource change script

---
+++ Assets/Scripts/Cards/CardResourceChange.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using TMPro;
+
+public class CardResourceChange : MonoBehaviour, IUpdateCardStats
+{
+	public ResourceType Type;
+
+	[SerializeField]
+	private TMP_Text _text;
+	public TMP_Text Text
+	{
+		get
+		{
+			if (this._text == null)
+				this._text = this.GetComponentInChildren<TMP_Text>();
+			return this._text;
+		}
+		set => this._text = value;
+	}
+
+	public Color PositiveColor = Color.green;
+
+	public Color NegativeColor = Color.red;
+
+	public void UpdateCardStats(CardStats stats)
+	{
+		int value;
+		bool has_value = stats.Changes.TryGetValue(this.Type, out value);
+		bool is_active = !has_value || value == 0;
+		this.gameObject.SetActive(is_active);
+		if (!is_active)
+			return;
+		this.Text.text = value.ToString();
+		this.Text.color = value < 0 ? this.NegativeColor : this.PositiveColor;
+	}
+}
