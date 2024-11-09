656898944a2dc7ad8eed1bef3662eeb8145653b1
dGameBoy101b <rhysmader@gmail.com>
2024-11-09 13:14:27 +0800

    Added costs and changes dictionaries to card stats.

--- Assets/Scripts/CardStats.cs
+++ Assets/Scripts/CardStats.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu()]
@@ -5,14 +7,88 @@ public class CardStats : ScriptableObject
 {
 	public string DisplayName;
 
-	[Min(0)]
-	public int TimeCost;
-
 	public Sprite Image;
 
-	public int FoodChange = 0;
+	[Serializable]
+	public struct ResourceChange
+	{
+		public ResourceType Type;
 
-	public int MoneyChange = 0;
+		public int Change;
+	}
+	public List<ResourceChange> ChangeList = new();
 
-	public int PassionChnage = 0;
+	#region Costs
+	private Dictionary<ResourceType, int> _costs = null;
+	public IReadOnlyDictionary<ResourceType, int> Costs
+	{
+		get
+		{
+			if (this._costs == null)
+				this._costs = this.CalculateCosts();
+			return this._costs;
+		}
+	}
+
+	private Dictionary<ResourceType, int> CalculateCosts()
+	{
+		Dictionary<ResourceType, int> costs = new();
+		foreach (ResourceChange change in this.ChangeList)
+		{
+			if (change.Change >= 0)
+				continue;
+			if (costs.ContainsKey(change.Type))
+				costs[change.Type] -= change.Change;
+			else
+				costs[change.Type] = -change.Change;
+		}
+		return costs;
+	}
+
+	public void InvalidateCosts()
+	{
+		this._costs = null;
+	}
+
+	public bool CanAfford()
+	{
+		return ResourceTracker.CanAfford(this.Costs);
+	}
+	#endregion
+
+	#region Changes
+	private Dictionary<ResourceType, int> _changes = null;
+	public IReadOnlyDictionary<ResourceType, int> Changes
+	{
+		get
+		{
+			if (this._changes == null)
+				this._changes = this.CalculateChanges();
+			return this._changes;
+		}
+	}
+
+	private Dictionary<ResourceType, int> CalculateChanges()
+	{
+		Dictionary<ResourceType, int> changes = new();
+		foreach (var item in this.ChangeList)
+		{
+			if (changes.ContainsKey(item.Type))
+				changes[item.Type] += item.Change;
+			else
+				changes.Add(item.Type, item.Change);
+		}
+		return changes;
+	}
+
+	public void InvalidateChanges()
+	{
+		this._changes = null;
+	}
+
+	public void ApplyChanges()
+	{
+		ResourceTracker.Change(this.Changes);
+	}
+	#endregion
 }
